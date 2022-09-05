15167d6f85246a80fc8f12a040d03bad140a6257
David Coelho <davidsocoelho@outlook.com>
2022-09-05 02:46:35 -0300

    Header adicionado

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3000
 const characters = require("./src/characters/characters.json")
 
 app.get('/characters', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   return res.json({characters})
 })
 
